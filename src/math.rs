5c149702507187ced4b08927750c7e9988448e0f
hickVieira <lhlvieirag@gmail.com>
2024-06-22 12:18:38 -0300

    added basic math library

---
+++ src/math.rs
@@ -0,0 +1,15 @@
+pub struct float2 {
+    x: f32,
+    y: f32,
+}
+
+pub struct float3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+pub fn run() {
+    let a = float2 { x: 1.0, y: 2.0 };
+    let b = float3 { x: 1.0, y: 2.0, z: 3.0 };
+}
