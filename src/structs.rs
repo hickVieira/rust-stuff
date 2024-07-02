443f72485cf0486172e89d12f8990be9d6aabf67
hickVieira <lhlvieirag@gmail.com>
2024-07-02 11:49:16 -0300

    added generic structs

--- src/structs.rs
+++ src/structs.rs
@@ -12,6 +12,31 @@ struct float2(f32, f32);
 struct float3(f32, f32, f32);
 struct NoDataStruct;
 
+// -----------------------
+
+trait MyTrait {
+    fn MyMethod(&self) -> &String;
+}
+
+struct MyStruct<T>
+where
+    T: MyTrait,
+{
+    name: String,
+    trait_type: T,
+}
+
+impl<T> MyStruct<T>
+where
+    T: MyTrait,
+{
+    fn MyMethod(&self) -> &String {
+        self.trait_type.MyMethod()
+    }
+}
+
+// -----------------------
+
 pub fn run() {
     let s = String::from("Hello world"); // static methods use ::
     let s_len = s.len(); // instance methods use .
