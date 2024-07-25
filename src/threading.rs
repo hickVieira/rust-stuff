5a35e6a3c604a56300e06d9fd4fb2fa9a21271da
hickVieira <lhlvieirag@gmail.com>
2024-07-25 15:55:29 -0300

    added more thread and parallel tests

--- src/threading.rs
+++ src/threading.rs
@@ -1,8 +1,10 @@
 use crossbeam_utils::thread;
 use jobsys;
 use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
+use threadpool::ThreadPool;
 
-const ARRAY_SIZE: usize = 1024 * 1024 * 512;
+const ARRAY_SIZE: usize = 1024 * 1024 * 1024;
 
 fn jobsys_for_each(thread_count: usize, job_capacity: usize) {
     let job_system = jobsys::JobSystem::new(thread_count, job_capacity).unwrap();
@@ -35,7 +37,7 @@ fn crossbeam_spawn(chunk_count: usize) {
     thread::scope(|s| {
         for chunk in array.chunks_mut(chunk_size) {
             s.spawn(|_| {
-                for item in chunk.iter_mut() {
+                for item in chunk {
                     *item = rand::random::<u8>();
                 }
             });
@@ -46,6 +48,93 @@ fn crossbeam_spawn(chunk_count: usize) {
 
     println!(
         "crossbeam.scope ({} chunks) : {}ms",
+        chunk_count,
+        duration.as_millis()
+    );
+}
+
+async fn tokio_spawn(chunk_count: usize) {
+    let array = vec![0u8; ARRAY_SIZE];
+    let chunk_size = array.len() / chunk_count;
+
+    let array = Arc::new(Mutex::new(vec![0u8; ARRAY_SIZE]));
+
+    let mut handles = vec![];
+
+    let timer = std::time::Instant::now();
+    for i in 0..chunk_count {
+        let array_clone = array.clone();
+        let handle = tokio::task::spawn(async move {
+            let start = i * chunk_size;
+            let mut array = array_clone.lock().unwrap();
+            let end = std::cmp::min(start + chunk_size, array.len());
+            for i in start..end {
+                array[i] = rand::random::<u8>();
+            }
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
+    let duration = timer.elapsed();
+
+    println!(
+        "tokio async ({} chunks) : {}ms",
+        chunk_count,
+        duration.as_millis()
+    );
+}
+
+fn threadpool_spawn(worker_count: usize, chunk_count: usize) {
+    let array = vec![0u8; ARRAY_SIZE];
+    let chunk_size = array.len() / chunk_count;
+
+    let pool = ThreadPool::new(worker_count);
+
+    let mut array = Arc::new(Mutex::new(array));
+
+    let timer = std::time::Instant::now();
+    for i in 0..chunk_count {
+        let array_clone = array.clone();
+        pool.execute(move || {
+            let start = i * chunk_size;
+            let mut array = array_clone.lock().unwrap();
+            let end = std::cmp::min(start + chunk_size, array.len());
+            for i in start..end {
+                array[i] = rand::random::<u8>();
+            }
+        });
+    }
+    pool.join();
+    let duration = timer.elapsed();
+
+    println!(
+        "threadpool ({} workers, {} chunks) : {}ms",
+        worker_count,
+        chunk_count,
+        duration.as_millis()
+    );
+}
+
+fn rayon_spawn(chunk_count: usize) {
+    let mut array = vec![0u8; ARRAY_SIZE];
+    let chunk_size = array.len() / chunk_count;
+
+    let timer = std::time::Instant::now();
+    rayon::scope(|s| {
+        for chunk in array.chunks_mut(chunk_size) {
+            s.spawn(|_| {
+                for item in chunk {
+                    *item = rand::random::<u8>();
+                }
+            })
+        }
+    });
+    let duration = timer.elapsed();
+
+    println!(
+        "rayon ({} chunks) : {}ms",
         chunk_count,
         duration.as_millis()
     );
@@ -70,9 +159,7 @@ pub async fn run() {
         let mut array = vec![0u8; ARRAY_SIZE];
 
         let timer = std::time::Instant::now();
-        array
-            .iter_mut()
-            .for_each(|i| *i = rand::random::<u8>());
+        array.iter_mut().for_each(|i| *i = rand::random::<u8>());
         let duration = timer.elapsed();
 
         println!("iter_mut : {}ms", duration.as_millis());
@@ -86,7 +173,7 @@ pub async fn run() {
         crossbeam_spawn(8); // fastest (prob related to cpu)
         crossbeam_spawn(16); // fast
         crossbeam_spawn(32);
-        crossbeam_spawn(64); 
+        crossbeam_spawn(64);
         crossbeam_spawn(128);
         crossbeam_spawn(256);
         crossbeam_spawn(512);
@@ -105,75 +192,79 @@ pub async fn run() {
         println!("rayon.par_iter_mut : {}ms", duration.as_millis());
     }
 
-    // spawn tokio sync
+    // rayon manual spawn
     {
-        let mut array = vec![0u8; ARRAY_SIZE];
-
-        let timer = std::time::Instant::now();
-        let task = tokio::task::spawn_blocking(move || {
-            array
-                .iter_mut()
-                .for_each(|i| *i = rand::random::<u8>());
-        });
-        task.await.unwrap();
-        let duration = timer.elapsed();
-
-        println!("tokio sync : {}ms", duration.as_millis());
+        rayon_spawn(1);
+        rayon_spawn(2);
+        rayon_spawn(4);
+        rayon_spawn(8);
+        rayon_spawn(16);
+        rayon_spawn(32);
+        rayon_spawn(64);
+        rayon_spawn(128);
+        rayon_spawn(256);
+        rayon_spawn(512);
     }
 
-    // spawn tokio async
-    {
-        let mut array = vec![0u8; ARRAY_SIZE];
+    // // spawn tokio
+    // {
+    //     tokio_spawn(1).await;
+    //     tokio_spawn(2).await;
+    //     tokio_spawn(4).await;
+    //     tokio_spawn(8).await;
+    //     tokio_spawn(16).await;
+    //     tokio_spawn(32).await;
+    //     tokio_spawn(64).await;
+    //     tokio_spawn(128).await;
+    //     tokio_spawn(256).await;
+    //     tokio_spawn(512).await;
+    // }
 
-        let timer = std::time::Instant::now();
-        tokio::task::spawn(async move {
-            array
-                .iter_mut()
-                .for_each(|i| *i = rand::random::<u8>());
-        })
-        .await
-        .unwrap();
-        let duration = timer.elapsed();
+    // // spawn threadpool
+    // {
+    //     threadpool_spawn(1, 1);
+    //     threadpool_spawn(1, 2);
+    //     threadpool_spawn(1, 4);
+    //     threadpool_spawn(1, 8);
+    //     threadpool_spawn(1, 16);
+    //     threadpool_spawn(1, 32);
+    //     threadpool_spawn(1, 64);
+    //     threadpool_spawn(1, 128);
+    //     threadpool_spawn(1, 256);
+    //     threadpool_spawn(1, 512);
 
-        println!("tokio async : {}ms", duration.as_millis());
-    }
+    //     threadpool_spawn(4, 1);
+    //     threadpool_spawn(4, 2);
+    //     threadpool_spawn(4, 4);
+    //     threadpool_spawn(4, 8);
+    //     threadpool_spawn(4, 16);
+    //     threadpool_spawn(4, 32);
+    //     threadpool_spawn(4, 64);
+    //     threadpool_spawn(4, 128);
+    //     threadpool_spawn(4, 256);
+    //     threadpool_spawn(4, 512);
+    // }
 
-    // spawn jobsys jobs
-    {
-        // jobsys_for_each(0, 2);
-        // jobsys_for_each(0, 4);
-        // jobsys_for_each(0, 8);
-        // jobsys_for_each(0, 16);
-        // jobsys_for_each(0, 32);
-        // jobsys_for_each(0, 64);
-        // jobsys_for_each(0, 128);
-        // jobsys_for_each(0, 256);
-        // jobsys_for_each(0, 512);
-        // jobsys_for_each(0, 1024);
+    // // spawn jobsys jobs
+    // {
+    //     jobsys_for_each(0, 2);
+    //     jobsys_for_each(0, 4);
+    //     jobsys_for_each(0, 8);
+    //     jobsys_for_each(0, 16);
+    //     jobsys_for_each(0, 32);
+    //     jobsys_for_each(0, 64);
+    //     jobsys_for_each(0, 128);
+    //     jobsys_for_each(0, 256);
+    //     jobsys_for_each(0, 512);
+    //     jobsys_for_each(0, 1024);
 
-        // jobsys_for_each(0, ARRAY_SIZE);
-        // jobsys_for_each(0, ARRAY_SIZE / 2);
-        // jobsys_for_each(0, ARRAY_SIZE / 4);
-        // jobsys_for_each(0, ARRAY_SIZE / 8);
-        // jobsys_for_each(0, ARRAY_SIZE / 16);
-        // jobsys_for_each(0, ARRAY_SIZE / 32);
-        // jobsys_for_each(0, ARRAY_SIZE / 64);
-
-        // jobsys_for_each(4, 8);
-        // jobsys_for_each(4, 16);
-        // jobsys_for_each(4, 32);
-        // jobsys_for_each(4, 64);
-        // jobsys_for_each(4, 128);
-        // jobsys_for_each(4, 256);
-        // jobsys_for_each(4, 512);
-        // jobsys_for_each(4, 1024);
-
-        // jobsys_for_each(4, ARRAY_SIZE);
-        // jobsys_for_each(4, ARRAY_SIZE / 2);
-        // jobsys_for_each(4, ARRAY_SIZE / 4);
-        // jobsys_for_each(4, ARRAY_SIZE / 8);
-        // jobsys_for_each(4, ARRAY_SIZE / 16);
-        // jobsys_for_each(4, ARRAY_SIZE / 32);
-        // jobsys_for_each(4, ARRAY_SIZE / 64);
-    }
+    //     jobsys_for_each(4, 8);
+    //     jobsys_for_each(4, 16);
+    //     jobsys_for_each(4, 32);
+    //     jobsys_for_each(4, 64);
+    //     jobsys_for_each(4, 128);
+    //     jobsys_for_each(4, 256);
+    //     jobsys_for_each(4, 512);
+    //     jobsys_for_each(4, 1024);
+    // }
 }
